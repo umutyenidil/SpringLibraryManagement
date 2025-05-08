4f4cf25426e0c8b578b0505d0d7f5ba39c0e1cb3
Umut Yenidil <umut.yenidil9@gmail.com>
2025-05-09 02:53:11 +0300

    auth controller'lar birlestirildi

--- services/user/src/main/java/com/umutyenidil/librarymanagement/auth/AuthController.java
+++ services/user/src/main/java/com/umutyenidil/librarymanagement/auth/AuthController.java
@@ -4,15 +4,24 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/librarian/v1/auth")
+@RequestMapping("/api/v1/auth")
 @RestController
-public class LibrarianAuthController {
+public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegisterRequest request
+    ) {
+        authService.registerPatron(request);
+
+        return ResponseEntity.accepted().build();
+    }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
--- services/user/src/main/java/com/umutyenidil/librarymanagement/security/SecurityConfig.java
+++ services/user/src/main/java/com/umutyenidil/librarymanagement/security/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                                 req.requestMatchers(
-                                        "/api/patron/v1/auth/**",
-                                        "/api/librarian/v1/auth/**"
+                                        "/api/v1/auth/**"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
