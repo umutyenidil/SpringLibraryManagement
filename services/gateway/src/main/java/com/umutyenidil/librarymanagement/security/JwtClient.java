77e47b2675aaa19a14eada593e74f4a658ddf864
Umut Yenidil <umut.yenidil9@gmail.com>
2025-05-12 22:39:42 +0300

    gateway'deki jwt client duzenlendi

--- services/gateway/src/main/java/com/umutyenidil/librarymanagement/security/JwtClient.java
+++ services/gateway/src/main/java/com/umutyenidil/librarymanagement/security/JwtClient.java
@@ -2,7 +2,9 @@ package com.umutyenidil.librarymanagement.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -18,14 +20,29 @@ public class JwtClient {
 
     private final WebClient.Builder webClientBuilder;
 
-    public Mono<Map> validateToken(String token) {
+    public Mono<Map<String, Object>> validateToken(String token) {
         return webClientBuilder.build()
                 .get()
                 .uri("http://USER-SERVICE/api/v1/auth/validate")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .retrieve()
-                .bodyToMono(Map.class)
-                .onErrorResume(WebClientResponseException.Unauthorized.class, ex -> Mono.error(new RuntimeException("Invalid token")))
-                .onErrorResume(WebClientResponseException.class, ex -> Mono.error(new RuntimeException("Error contacting user-service")));
+                .onStatus(
+                        status -> status != null && status.is4xxClientError(),
+                        response -> {
+
+                            if (HttpStatus.UNAUTHORIZED.equals(response.statusCode())) {
+                                return Mono.error(new RuntimeException("Invalid token"));
+                            }
+
+                            return Mono.error(new RuntimeException("Client error during token validation"));
+                        })
+                .onStatus(
+                        status -> status != null && status.is5xxServerError(),
+                        response ->
+                                Mono.error(new RuntimeException("User-service is currently unavailable"))
+                )
+                .bodyToMono(new ParameterizedTypeReference<>() {
+                });
     }
+
 }
